362545f569baea43eb0558ee410ba83af7ae83bf
Exoticdrey <150359600+Exoticdrey@users.noreply.github.com>
2025-08-11 20:48:40 +0100

    Delete blog/Static directory

--- blog/Static/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function (event) {
-  console.log('[ServiceWorker] Installed');
-});
-
-self.addEventListener('fetch', function (event) {
-  // Optional caching logic
-});
