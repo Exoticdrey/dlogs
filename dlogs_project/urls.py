f3931b6fcf51bc132ee0e5ec58a8731a66e25985
Exoticdrey <150359600+Exoticdrey@users.noreply.github.com>
2025-08-12 00:53:27 +0100

    Update urls.py

--- dlogs_project/urls.py
+++ dlogs_project/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from yourapp.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
 
 # Serve media files in development
