e7fa8c64c184bf75aa47f30ea0efa3d40326a3c7
Exoticdrey <150359600+Exoticdrey@users.noreply.github.com>
2025-08-11 19:20:09 +0100

    Update settings.py

--- dlogs_project/settings.py
+++ dlogs_project/settings.py
@@ -133,6 +133,12 @@ STATIC_URL = '/static/'
 # ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
