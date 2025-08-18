2ca1e07de88c7230bb52c1d58dc57f1468fd2905
Exoticdrey <150359600+Exoticdrey@users.noreply.github.com>
2025-08-18 20:53:42 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('like/<slug:post_slug>/', views.like_post, name='like_post'),
     path('category/<slug:slug>/', views.category_posts, name='category_posts'),
     path('search/', views.search_posts, name='search'),
 
